565edb2a6579d7ac39fae48df604e474514dc024
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-05-07 08:00:46 +0500

    Media Queries Applied

--- src/components/ContactSection/ContactSection.js
+++ src/components/ContactSection/ContactSection.js
@@ -10,31 +10,33 @@ function ContactSection() {
 
             <div className="contact">
                 <div className="contact__detailBox">
-                    <div className="contact__detail">
-                        <a href="tel:+92-306-2435762" className="roundIcon"><i class="fas fa-phone"></i></a>
-                        <div>
-                            <div className="contact__name">Phone</div>
-                            <div className="contact__info"><a href="tel:+92-306-2435762">+92-306-2435762</a></div>
+                    <div className="contact__detailSeperator">
+                        <div className="contact__detail">
+                            <a href="tel:+92-306-2435762" className="roundIcon"><i class="fas fa-phone"></i></a>
+                            <div>
+                                <div className="contact__name">Phone</div>
+                                <div className="contact__info"><a href="tel:+92-306-2435762">+92-306-2435762</a></div>
+                            </div>
+                        </div>
+
+                        <div className="contact__detail">
+                            <a href="mailto:usama.zuberi1010@gmail.com" className="roundIcon"><i class="fas fa-envelope"></i></a>
+                            <div>
+                                <div className="contact__name">Email</div>
+                                <div className="contact__info"><a href="mailto:usama.zuberi1010@gmail.com">usama.zuberi1010@gmail.com</a></div>
+                            </div>
+                        </div>
+
+                        <div className="contact__detail">
+                            <a href="https://goo.gl/maps/LJ7cLaTn9qMTeJpA9" className="roundIcon"><i class="fas fa-map-marker-alt"></i></a>
+                            <div>
+                                <div className="contact__name">Address</div>
+                                <div className="contact__info"><a href="https://goo.gl/maps/LJ7cLaTn9qMTeJpA9" rel="noreferrer" target="_blank">Karachi, Pakistan</a></div>
+                            </div>
                         </div>
                     </div>
 
-                    <div className="contact__detail">
-                        <a href="mailto:usama.zuberi1010@gmail.com" className="roundIcon"><i class="fas fa-envelope"></i></a>
-                        <div>
-                            <div className="contact__name">Email</div>
-                            <div className="contact__info"><a href="mailto:usama.zuberi1010@gmail.com">usama.zuberi1010@gmail.com</a></div>
-                        </div>
-                    </div>
-
-                    <div className="contact__detail">
-                        <a href="https://goo.gl/maps/LJ7cLaTn9qMTeJpA9" className="roundIcon"><i class="fas fa-map-marker-alt"></i></a>
-                        <div>
-                            <div className="contact__name">Address</div>
-                            <div className="contact__info"><a href="https://goo.gl/maps/LJ7cLaTn9qMTeJpA9" rel="noreferrer" target="_blank">Karachi, Pakistan</a></div>
-                        </div>
-                    </div>
-
-                    <div >
+                    <div className="contact__socialBox">
                         <h4 className="contact__heading">Connect with me</h4>
                         <div className="contact__social">
                             <a href="https://www.facebook.com/muhammadusama.zuberi" target="_blank" rel="noreferrer" className="roundIcon"><i class="fab fa-facebook-f"></i></a>
