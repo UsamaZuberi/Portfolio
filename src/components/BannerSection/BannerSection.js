fea9d310dd32766ca9252465b95d52be4f3a9a1e
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-05-07 05:23:56 +0500

    Smooth Scroll Added

--- src/components/BannerSection/BannerSection.js
+++ src/components/BannerSection/BannerSection.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { Link } from "react-scroll";
+
 import './BannerSection.scss';
+
 import BannerImage from '../../assets/images/bannerImg.png';
 import CV from '../../assets/cv/Muhammad_Usama_Zuberi_CV.pdf';
 import Button from '../Button/Button';
@@ -9,12 +12,6 @@ function BannerSection() {
     return (
         <section className="bannerSection">
             <div className="bannerSection__left">
-                {/* <div className="socialLinks">
-                    <span className="socialLinks__icon"><i class="fab fa-facebook-f"></i></span>
-                    <span className="socialLinks__icon"><i class="fab fa-linkedin-in"></i></span>
-                    <span className="socialLinks__icon"><i class="fab fa-twitter"></i></span>
-                    <span className="socialLinks__icon"><i class="fab fa-whatsapp"></i></span>
-                </div> */}
                 <div className="introText">
                     <span className="introText__salutation">Hello! I'm</span>
                     <h1 className="introText__name">Muhammad Usama Zuberi</h1>
@@ -23,22 +20,21 @@ function BannerSection() {
                     <a href={CV} download="Muhammad_Usama_Zuberi_CV.pdf">
                         <Button type="button" text="Download CV" onClick={() => { }} />
                     </a>
-
-                    {/* <div className="bannerButtons">
-                        <a href={CV} download="Muhammad_Usama_Zuberi_CV.pdf">
-                            <Button type="button" text="Download CV" onClick={() => { }} />
-                        </a>
-
-                        <a href="mailto:usama.zuberi1010@gmail.com">
-                            <Button type="button" text="Email Me" onClick={() => { }} />
-                        </a>
-                    </div> */}
                 </div>
             </div>
             <div className="bannerSection__right">
                 <img src={BannerImage} alt="Muhammad Usama Zuberi" className="bannerImg" />
             </div>
-            <a href="./#portfolioSection" className="mouseScroll"><i class="fas fa-angle-double-down"></i></a>
+            <Link
+                activeClass="active"
+                to="portfolioSection"
+                spy={true}
+                smooth={true}
+                duration={500}
+                className="mouseScroll"
+            >
+                <i class="fas fa-angle-double-down"></i>
+            </Link>
         </section>
     )
 }
