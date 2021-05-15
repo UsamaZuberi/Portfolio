61cd93c0b6f4bd39566477fea132981f088e07a1
Muhammad Usama Zuberi <usama.zuberi1010@gmail.com>
2021-05-15 17:18:25 +0500

    Fixes For Safari & CV Updated

--- src/components/PortfolioSection/PortfolioSection.js
+++ src/components/PortfolioSection/PortfolioSection.js
@@ -24,7 +24,7 @@ function PortfolioSection() {
                     to="contactSection"
                     spy={true}
                     smooth={true}
-                    duration={500}
+                    duration={600}
                     className="emailLink"
                 >
                     &nbsp;Email me
@@ -33,14 +33,14 @@ function PortfolioSection() {
 
             <div className="cardsGallery">
                 <PortfolioCards logo={Pixtool} title="Pixtool" description="A Saas web app for video editing, sharing & stage simulation." link="https://pixtool-66579.firebaseapp.com/home" />
-                <PortfolioCards logo={SevenStarTraining} title="7 Star Training" description="A Saas web app for apprentiseship trainings." link="https://star-309014.ew.r.appspot.com/" />
-                <PortfolioCards logo={Novospace} title="Novospace" description="A Saas web app for Novospace for hardware & products configurations." link="https://novo-back.herokuapp.com/" />
                 <PortfolioCards logo={EberhardCapital} title="Eberhard Capital" description="Official website of  Eberhard Capital." link="http://eberhardcapital.com/" />
                 <PortfolioCards logo={EHJAndSJConsultancy} title="EHJ & SJ Contultancy" description="Official website of EHJ & SJ Contultancy." link="https://ehjsjconsultancy.com/" />
-                <PortfolioCards logo="" title="Cylinder" description="A website for complex daily hot water requirements calculation of people." link="https://cylinder-41ed8.firebaseapp.com/" />
                 <PortfolioCards logo={Natours} title="Natours" description="A website for fictional tour company Natours" link="https://usamazuberi.github.io/Natours/" />
+                <PortfolioCards logo={SevenStarTraining} title="7 Star Training" description="A Saas web app for apprentiseship trainings." link="https://star-309014.ew.r.appspot.com/" />
                 <PortfolioCards logo={Nexter} title="Nexter" description="A website for fictional real state company Nexter" link="https://usamazuberi.github.io/Nexter/" />
                 <PortfolioCards logo={Trillo} title="Trillo" description="A website for fictional all in one tour company Trillo" link="https://usamazuberi.github.io/Trillo/" />
+                <PortfolioCards logo="" title="Cylinder" description="A website for complex daily hot water requirements calculation of people." link="https://cylinder-41ed8.firebaseapp.com/" />
+                <PortfolioCards logo={Novospace} title="Novospace" description="A Saas web app for Novospace for hardware & products configurations." link="https://novo-back.herokuapp.com/" />
             </div>
         </section>
     )
